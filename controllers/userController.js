fb9f67d90062f1af2cbb503179c0034dd90cd0a3
Chase Wickett <wickettc@yahoo.com>
2021-02-01 22:58:40 -0500

    working on signup validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,22 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.sign_up_get = (req, res) => {
     res.render('sign_up');
 };
 
-exports.sign_up_post = (req, res) => {
-    res.send('sign up post');
+exports.sign_up_post = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, (error, hashedPW) => {
+        const user = new User({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            username: req.body.username,
+            password: hashedPW,
+        }).save((err) => {
+            if (err) return next(err);
+            res.redirect('/');
+        });
+    });
 };
 
 exports.log_in_get = (req, res) => {
--- models/user.js
+++ models/user.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    first_name: { type: String, required: true, maxlength: 50 },
-    last_name: { type: String, required: true, maxlength: 50 },
+    firstname: { type: String, required: true, maxlength: 50 },
+    lastname: { type: String, required: true, maxlength: 50 },
     username: { type: String, required: true, maxlength: 50 },
-    password: { type: String, required: true, maxlength: 50 },
+    password: { type: String, required: true },
 });
 
 UserSchema.virtual('fullName').get(() => this.first_name + this.last_name);
